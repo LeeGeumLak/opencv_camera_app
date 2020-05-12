170c3278ebfdf51017a1238485fb8ac65fb93091
LeeGeumLak <ekdma4105@naver.com>
2020-05-12 13:53:25 +0900

    전화 올 때의 액티비티인 IncomingCallActivity 추가

---
+++ app/src/main/java/com/example/lglcamera/IncomingCallActivity.java
@@ -0,0 +1,13 @@
+package com.example.lglcamera;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class IncomingCallActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_incoming_call);
+    }
+}
