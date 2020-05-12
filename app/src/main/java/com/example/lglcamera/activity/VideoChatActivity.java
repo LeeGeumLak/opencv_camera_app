8c5181808ff93be1246256142644f14eb5054976
LeeGeumLak <ekdma4105@naver.com>
2020-05-12 15:15:08 +0900

    화상채팅을 위한 액티비티, 어댑터, 데이터, 뷰 아웃라인 추가

---
+++ app/src/main/java/com/example/lglcamera/activity/VideoChatActivity.java
@@ -0,0 +1,14 @@
+package com.example.lglcamera.activity;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.lglcamera.R;
+
+public class VideoChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_chat);
+    }
+}
