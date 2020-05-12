e725b15e7790612cfbeb68cbd0aec9fe8653a731
LeeGeumLak <ekdma4105@naver.com>
2020-05-12 13:46:57 +0900

    화상채팅 메인 액티비티 생성

---
+++ app/src/main/java/com/example/lglcamera/FaceTalkActivity.java
@@ -0,0 +1,13 @@
+package com.example.lglcamera;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FaceTalkActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_face_talk);
+    }
+}
