134f271202add3e16cba7217f7dc5993b7cf4a79
LeeGeumLak <ekdma4105@naver.com>
2020-05-18 17:20:50 +0900

    ThrowableExtension.java 추가( for 화상통화 기능 에러 )

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -374,112 +374,94 @@ Java_com_example_lglcamera_activity_MainActivity_DetectAndDraw (JNIEnv *env, job
     Mat &img_result = *(Mat *) mat_addr_Result;
     img_result = img_input.clone();
 
-    //Mat output2;
-    //img_result.copyTo(output2);
+    Mat output2;
+    img_result.copyTo(output2);
 
-    String glassesImage = "D:/A/teamnova_basic_project/basic_android_second_chance/teamnova_basic_project_android_1st/app/src/main/assets/sunglasses.png";
+
+    //---------------------------------------------------------------------------------
+    //__android_log_print(ANDROID_LOG_DEBUG, (char *) "native-lib :: ","%d", 2);
+
+    vector<Rect> faces;
+    Mat img_gray;
+
+    //__android_log_print(ANDROID_LOG_DEBUG, (char *) "native-lib :: ","%d", 3);
+
+    cvtColor(img_input, img_gray, COLOR_BGR2GRAY);
+    equalizeHist(img_gray, img_gray);
+    Mat img_resize;
+    float resizeRatio = imgResize(img_gray, img_resize, 640);
+
+    //__android_log_print(ANDROID_LOG_DEBUG, (char *) "native-lib :: ","%d", 4);
+
+    //-- Detect faces
+    ((CascadeClassifier *) cascadeClassifier_face)->detectMultiScale( img_resize, faces, 1.1, 2, 0|CASCADE_SCALE_IMAGE, Size(30, 30) );
+
+    //__android_log_print(ANDROID_LOG_DEBUG, (char *) "native-lib :: ", (char *) "face %d found ", faces.size());
+
+    //__android_log_print(ANDROID_LOG_DEBUG, (char *) "native-lib :: ","%d", 5);
+
+    for (int i = 0; i < faces.size(); i++) {
+        double real_facesize_x = faces[i].x / resizeRatio;
+        double real_facesize_y = faces[i].y / resizeRatio;
+        double real_facesize_width = faces[i].width / resizeRatio;
+        double real_facesize_height = faces[i].height / resizeRatio;
+
+        Point center( real_facesize_x + real_facesize_width / 2, real_facesize_y + real_facesize_height/2);
+
+        ellipse(img_result, center, Size( real_facesize_width / 2, real_facesize_height / 2), 0, 0, 360, Scalar(255, 192, 0), 4, 8, 0);
+
+        Rect face_area(real_facesize_x, real_facesize_y, real_facesize_width,real_facesize_height);
+
+        Mat faceROI = img_gray( face_area );
+
+        vector<Rect> eyes;
+
+        //-- In each face, detect eyes
+        ((CascadeClassifier *) cascadeClassifier_eye)->detectMultiScale( faceROI, eyes, 1.1, 2, 0 |CASCADE_SCALE_IMAGE, Size(15, 15) );
+        for ( size_t j = 0; j < eyes.size(); j++ ) {
+            Point eye_center( real_facesize_x + eyes[j].x + eyes[j].width/2, real_facesize_y + eyes[j].y + eyes[j].height/2 );
+
+            int radius = cvRound( (eyes[j].width + eyes[j].height)*0.25 );
+            circle( img_result, eye_center, radius, Scalar( 89, 89, 89 ), 4, 8, 0 );
+            /*Mat &img_input = *(Mat *) mat_addr_Input;
+            Mat &img_result = *(Mat *) mat_addr_Result;
+
+            img_result = img_input;
+
+            String glassesImage = "sunglasses.png";
+            bool tryflip = false;
+            double scale;
+            scale = 1;
+
+            Mat glasses = imread(glassesImage, IMREAD_UNCHANGED);
+
+            CascadeClassifier cascade, nestedCascade;
+            cascade = *(CascadeClassifier *)cascadeClassifier_face;
+            nestedCascade = *(CascadeClassifier *)cascadeClassifier_eye;
+
+            detectAndDraw(img_result, cascade, nestedCascade, scale, tryflip, glasses);*/
+        }
+    }
+
+    //__android_log_print(ANDROID_LOG_DEBUG, (char *) "native-lib :: ","%d", 6);*/
+
+    /*Mat &img_input = *(Mat *) mat_addr_Input;
+    Mat &img_result = *(Mat *) mat_addr_Result;
+
+    img_result = img_input;
+
+    String glassesImage = "sunglasses.png";
     bool tryflip = false;
     double scale;
     scale = 1;
+
+    Mat glasses = imread(glassesImage, IMREAD_UNCHANGED);
 
     CascadeClassifier cascade, nestedCascade;
     cascade = *(CascadeClassifier *)cascadeClassifier_face;
     nestedCascade = *(CascadeClassifier *)cascadeClassifier_eye;
 
-    Mat glasses;
-    glasses = imread(glassesImage, IMREAD_UNCHANGED);
-    if (glasses.empty()){
-
-        cout << "Could not read image - " << glassesImage << endl;
-        return -1;
-    }
-
-    detectAndSunglasses(img_result, cascade, nestedCascade, scale, tryflip, glasses);
-
-    //---------------------------------------------------------------------------------
-//    //__android_log_print(ANDROID_LOG_DEBUG, (char *) "native-lib :: ","%d", 2);
-//
-//    vector<Rect> faces;
-//    Mat img_gray;
-//
-//    //__android_log_print(ANDROID_LOG_DEBUG, (char *) "native-lib :: ","%d", 3);
-//
-//    cvtColor(img_input, img_gray, COLOR_BGR2GRAY);
-//    equalizeHist(img_gray, img_gray);
-//    Mat img_resize;
-//    float resizeRatio = imgResize(img_gray, img_resize, 640);
-//
-//    //__android_log_print(ANDROID_LOG_DEBUG, (char *) "native-lib :: ","%d", 4);
-//
-//    //-- Detect faces
-//    ((CascadeClassifier *) cascadeClassifier_face)->detectMultiScale( img_resize, faces, 1.1, 2, 0|CASCADE_SCALE_IMAGE, Size(30, 30) );
-//
-//    //__android_log_print(ANDROID_LOG_DEBUG, (char *) "native-lib :: ", (char *) "face %d found ", faces.size());
-//
-//    //__android_log_print(ANDROID_LOG_DEBUG, (char *) "native-lib :: ","%d", 5);
-//
-//    for (int i = 0; i < faces.size(); i++) {
-//        double real_facesize_x = faces[i].x / resizeRatio;
-//        double real_facesize_y = faces[i].y / resizeRatio;
-//        double real_facesize_width = faces[i].width / resizeRatio;
-//        double real_facesize_height = faces[i].height / resizeRatio;
-//
-//        Point center( real_facesize_x + real_facesize_width / 2, real_facesize_y + real_facesize_height/2);
-//
-//        ellipse(img_result, center, Size( real_facesize_width / 2, real_facesize_height / 2), 0, 0, 360, Scalar(255, 192, 0), 4, 8, 0);
-//
-//        Rect face_area(real_facesize_x, real_facesize_y, real_facesize_width,real_facesize_height);
-//
-//        Mat faceROI = img_gray( face_area );
-//
-//        vector<Rect> eyes;
-//
-//        //-- In each face, detect eyes
-//        ((CascadeClassifier *) cascadeClassifier_eye)->detectMultiScale( faceROI, eyes, 1.1, 2, 0 |CASCADE_SCALE_IMAGE, Size(15, 15) );
-//        for ( size_t j = 0; j < eyes.size(); j++ ) {
-//            Point eye_center( real_facesize_x + eyes[j].x + eyes[j].width/2, real_facesize_y + eyes[j].y + eyes[j].height/2 );
-//
-//            int radius = cvRound( (eyes[j].width + eyes[j].height)*0.25 );
-//            circle( img_result, eye_center, radius, Scalar( 89, 89, 89 ), 4, 8, 0 );
-//            /*Mat &img_input = *(Mat *) mat_addr_Input;
-//            Mat &img_result = *(Mat *) mat_addr_Result;
-//
-//            img_result = img_input;
-//
-//            String glassesImage = "sunglasses.png";
-//            bool tryflip = false;
-//            double scale;
-//            scale = 1;
-//
-//            Mat glasses = imread(glassesImage, IMREAD_UNCHANGED);
-//
-//            CascadeClassifier cascade, nestedCascade;
-//            cascade = *(CascadeClassifier *)cascadeClassifier_face;
-//            nestedCascade = *(CascadeClassifier *)cascadeClassifier_eye;
-//
-//            detectAndDraw(img_result, cascade, nestedCascade, scale, tryflip, glasses);*/
-//        }
-//    }
-//
-//    //__android_log_print(ANDROID_LOG_DEBUG, (char *) "native-lib :: ","%d", 6);*/
-//
-//    /*Mat &img_input = *(Mat *) mat_addr_Input;
-//    Mat &img_result = *(Mat *) mat_addr_Result;
-//
-//    img_result = img_input;
-//
-//    String glassesImage = "sunglasses.png";
-//    bool tryflip = false;
-//    double scale;
-//    scale = 1;
-//
-//    Mat glasses = imread(glassesImage, IMREAD_UNCHANGED);
-//
-//    CascadeClassifier cascade, nestedCascade;
-//    cascade = *(CascadeClassifier *)cascadeClassifier_face;
-//    nestedCascade = *(CascadeClassifier *)cascadeClassifier_eye;
-//
-//    detectAndDraw(img_result, cascade, nestedCascade, scale, tryflip, glasses);*/
+    detectAndDraw(img_result, cascade, nestedCascade, scale, tryflip, glasses);*/
 }
 
 // rgb -> gray
